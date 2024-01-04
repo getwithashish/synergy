9e0fc0ed5b7493667eeca9d6c57866c968fb9223
Arjun Sreekumar <arjun.sreekumar@experionglobal.local>
2024-01-04 13:55:19 +0530

    feat : profile page animation

--- pages/profile-page/cancellation.js
+++ pages/profile-page/cancellation.js
@@ -2,23 +2,19 @@ function submitCancellation() {
         // Fetch input values
         const pnrNumber = document.getElementById('pnrNumber').value;
         const trainNumber = document.getElementById('trainNumber').value;
-        const journeyDetails = document.getElementById('journeyDetails').value;
-        const passengerDetails = document.getElementById('passengerDetails').value;
-        const classOfTravel = document.getElementById('classOfTravel').value;
+        // const journeyDetails = document.getElementById('journeyDetails').value;
+        // const passengerDetails = document.getElementById('passengerDetails').value;
+        // const classOfTravel = document.getElementById('classOfTravel').value;
         const reasonForCancellation = document.getElementById('reasonForCancellation').value;
-        const contactInformation = document.getElementById('contactInformation').value;
-        const paymentDetails = document.getElementById('paymentDetails').value;
-
+        // const contactInformation = document.getElementById('contactInformation').value;
+        // const paymentDetails = document.getElementById('paymentDetails').value;
+        const contactInformation = document.getElementById("contactInformation").value;
         // Use Axios to send cancellation details to the server
         axios.post('/cancellation-endpoint', {
             pnrNumber,
             trainNumber,
-            journeyDetails,
-            passengerDetails,
-            classOfTravel,
             reasonForCancellation,
-            contactInformation,
-            paymentDetails
+            contactInformation
         })
         .then(function (response) {
             // Handle successful cancellation response
