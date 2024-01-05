ae077afcdc9fa249876b4a009e7bfb95d4761808
Ashish Sam T George <ashish.george@experionglobal.local>
2024-01-05 16:54:19 +0530

    fix: Refactor js files

--- src/searchStations.js
+++ src/searchStations.js
@@ -1,81 +1,81 @@
 const autoCompleteSrcStationJS = new autoComplete({
-    selector: "#autocompleteSrcStation",
-    placeHolder: "Source Station...",
-    data: {
-        src: async (query) => {
-            const data = await searchStation({query: query});
-            return data;
-        },
-        keys: ["name", "code"],
-        cache: false,
+  selector: "#autocompleteSrcStation",
+  placeHolder: "Source Station...",
+  data: {
+    src: async (query) => {
+      const data = await searchStation({ query: query });
+      return data;
     },
-    resultsList: {
-        element: (list, data) => {
-            if (!data.results.length) {
-                // Create "No Results" message element
-                const message = document.createElement("div");
-                // Add class to the created element
-                message.setAttribute("class", "no_result");
-                // Add message text content
-                message.innerHTML = `<span>Found No Results for "${data.query}"</span>`;
-                // Append message element to the results list
-                list.prepend(message);
-            }
-        },
-        noResults: true,
+    keys: ["name", "code"],
+    cache: false,
+  },
+  resultsList: {
+    element: (list, data) => {
+      if (!data.results.length) {
+        // Create "No Results" message element
+        const message = document.createElement("div");
+        // Add class to the created element
+        message.setAttribute("class", "no_result");
+        // Add message text content
+        message.innerHTML = `<span>Found No Results for "${data.query}"</span>`;
+        // Append message element to the results list
+        list.prepend(message);
+      }
     },
-    resultItem: {
-        highlight: true
+    noResults: true,
+  },
+  resultItem: {
+    highlight: true,
+  },
+  events: {
+    input: {
+      selection: (event) => {
+        const selection = event.detail.selection.value;
+        autoCompleteSrcStationJS.input.value = `${selection.name} (${selection.code})`;
+        addSourceMarker(selection.code);
+        console.log("Selected: ", selection);
+      },
     },
-    events: {
-        input: {
-            selection: (event) => {
-                const selection = event.detail.selection.value;
-                autoCompleteSrcStationJS.input.value = `${selection.name} (${selection.code})`;
-                addSourceMarker(selection.code);
-                console.log("Selected: ", selection)
-            }
-        }
-    }
+  },
 });
 
 const autoCompleteDestStationJS = new autoComplete({
-    selector: "#autocompleteDestStation",
-    placeHolder: "Destination Station...",
-    data: {
-        src: async (query) => {
-            const data = await searchStation({query: query});
-            return data;
-        },
-        keys: ["name", "code"],
-        cache: false,
+  selector: "#autocompleteDestStation",
+  placeHolder: "Destination Station...",
+  data: {
+    src: async (query) => {
+      const data = await searchStation({ query: query });
+      return data;
     },
-    resultsList: {
-        element: (list, data) => {
-            if (!data.results.length) {
-                // Create "No Results" message element
-                const message = document.createElement("div");
-                // Add class to the created element
-                message.setAttribute("class", "no_result");
-                // Add message text content
-                message.innerHTML = `<span>Found No Results for "${data.query}"</span>`;
-                // Append message element to the results list
-                list.prepend(message);
-            }
-        },
-        noResults: true,
+    keys: ["name", "code"],
+    cache: false,
+  },
+  resultsList: {
+    element: (list, data) => {
+      if (!data.results.length) {
+        // Create "No Results" message element
+        const message = document.createElement("div");
+        // Add class to the created element
+        message.setAttribute("class", "no_result");
+        // Add message text content
+        message.innerHTML = `<span>Found No Results for "${data.query}"</span>`;
+        // Append message element to the results list
+        list.prepend(message);
+      }
     },
-    resultItem: {
-        highlight: true
+    noResults: true,
+  },
+  resultItem: {
+    highlight: true,
+  },
+  events: {
+    input: {
+      selection: (event) => {
+        const selection = event.detail.selection.value;
+        autoCompleteDestStationJS.input.value = `${selection.name} (${selection.code})`;
+        addDestinationMarker(selection.code);
+        console.log("Selected: ", selection);
+      },
     },
-    events: {
-        input: {
-            selection: (event) => {
-                const selection = event.detail.selection.value;
-                autoCompleteDestStationJS.input.value = `${selection.name} (${selection.code})`;
-                addDestinationMarker(selection.code);
-                console.log("Selected: ", selection)
-            }
-        }
-    }
+  },
 });
