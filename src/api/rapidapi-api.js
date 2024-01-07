6793ae40a3207197cbe4000fbc34a72377f26bcd
Devapriya L <devapriya.l@experionglobal.local>
2024-01-07 19:26:23 +0530

    feat: Top routes page

--- src/api/rapidapi-api.js
+++ src/api/rapidapi-api.js
@@ -1,5 +1,5 @@
 const rapidapiHeaders = {
-    'X-RapidAPI-Key': '0a45d67800msh4c62ed430a92ae4p11eb71jsn26ab8bf20a00',
+    'X-RapidAPI-Key': 'd7fb7a003cmshc35ca485b57bd02p1bbe5bjsn426162ea0431',
     'X-RapidAPI-Host': 'irctc1.p.rapidapi.com'
 };
 
