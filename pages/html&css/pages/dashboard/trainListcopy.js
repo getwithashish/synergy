d425748a290bcbc2edbc13775b3938a66fd11ddb
Ashish Sam T George <ashish.george@experionglobal.local>
2024-01-08 16:45:50 +0530

    feat: Add Profile Button in Navbar

--- pages/html&css/pages/dashboard/trainListcopy.js
+++ pages/html&css/pages/dashboard/trainListcopy.js
@@ -52,7 +52,8 @@ async function searchSeat(){
   const urlParams = new URLSearchParams(window.location.search);
   const trainNumber = urlParams.get('trainNumber');
   
-  const seatAva = await checkSeatAvailability({ classType:classTypeInput,fromStationCode:fromInput, quota:quotaInput,toStationCode:toInput,trainNo:trainNumber, date:dateInput})
+  // const seatAva = await checkSeatAvailability({ classType:classTypeInput,fromStationCode:fromInput, quota:quotaInput,toStationCode:toInput,trainNo:trainNumber, date:dateInput})
+  const seatAva = await getDummySeatAvailability({ classType:classTypeInput,fromStationCode:fromInput, quota:quotaInput,toStationCode:toInput,trainNo:trainNumber, date:dateInput})
   .then((response) => {
     console.log("Inside seat availability: ", response);
     let seatArray = response;
